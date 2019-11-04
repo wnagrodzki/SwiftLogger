04ab08f29486b0ab35245986bd0b200b93e85d33
Wojciech Nagrodzki <278594+wnagrodzki@users.noreply.github.com>
2019-11-04 19:21:53 +0100

    Use new FileHandle methods that throw errors instead of exceptions

--- Logger/Loggers/DiskLogger/OSFileHandle.swift
+++ Logger/Loggers/DiskLogger/OSFileHandle.swift
@@ -9,15 +9,45 @@
 import Foundation
 
 protocol OSFileHandle {
-    func seekToEndOfFile() -> UInt64
-    func swift_write(_ data: Data) throws
-    func synchronizeFile()
-    func closeFile()
+    func osSeekToEndOfFile() throws -> UInt64
+    func osWrite(_ data: Data) throws
+    func osSynchronizeFile() throws
+    func osCloseFile() throws
 }
 
 extension FileHandle: OSFileHandle {
     
-    func swift_write(_ data: Data) throws {
-        try __write(data, error: ())
+    func osSeekToEndOfFile() throws -> UInt64 {
+        if #available(iOS 13.0, *) {
+            var offsetInFile: UInt64 = 0
+            try __seek(toEndReturningOffset:&offsetInFile)
+            return offsetInFile
+        } else {
+            fatalError()
+        }
+    }
+    
+    func osWrite(_ data: Data) throws {
+        if #available(iOS 13.0, *) {
+            try __write(data, error: ())
+        } else {
+            fatalError()
+        }
+    }
+    
+    func osSynchronizeFile() throws {
+        if #available(iOS 13.0, *) {
+            try synchronize()
+        } else {
+            fatalError()
+        }
+    }
+    
+    func osCloseFile() throws {
+        if #available(iOS 13.0, *) {
+            try close()
+        } else {
+            fatalError()
+        }
     }
 }
